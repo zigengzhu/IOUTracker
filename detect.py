6f8f024d9df8f74314800a569e31787d94f464a1
zigengzhu <chris.zhu1997@gmail.com>
2021-12-16 17:24:50 -0600

    commit

--- detect.py
+++ detect.py
@@ -1,135 +1,92 @@
+import os
 import time
 import cv2 as cv
-import yolo_detector
 
-new_boundaries = []
-
-
-def append_click_pos(event, x, y, flags, param):
-    if event == cv.EVENT_LBUTTONDBLCLK:
-        new_boundaries.append((x, y))
+color_dict = {0: (255, 255, 255), 1: (255, 0, 0), 2: (0, 255, 0), 3: (0, 0, 255), 5: (255, 255, 0), 7: (0, 255, 255)}
+label_dict = {0: "person", 1: "bicycle", 2: "car", 3: "motorcycle", 5: "bus", 7: "truck"}
 
 
-def draw_bbox(im, bboxes, blx, ury):
-    color_dict = {3: (255, 0, 0), 2: (0, 0, 255), 7: (0, 255, 0), 5: (0, 255, 255)}
-    label_dict = {2: "car", 3: "motorcycle", 5: "bus", 7: "truck"}
-    if len(new_boundaries) > 0:
-        for box in bboxes:
-            pt1 = (box[0] + blx, box[1] + ury)
-            pt2 = (box[2] + blx, box[3] + ury)
-            text_pos = (box[0] + blx, box[1] + ury - 2)
-            label = box[5]
-            id = box[4]
-            color = color_dict[box[5]]
-            im = cv.rectangle(im, pt1, pt2, color, 2)
-            cv.putText(im, 'ID-' + str(id) + ': ' + label_dict[label], text_pos, cv2.FONT_HERSHEY_SIMPLEX, 0.5,
-                       color, 1)
-    else:
-        for box in bboxes:
-            im = cv.rectangle(im, (box[0], box[1]), (box[2], box[3]), color_dict[box[5]], 2)
+def draw_bbox(im, bboxes, output, fcount):
+    drawn_ids = []
+    for box in bboxes:
+        pt1 = (int(box[0]), int(box[1]))
+        pt2 = (int(box[2]), int(box[3]))
+        text_pos = (int(box[0]), int(box[1]) - 2)
+        label = box[5]
+        id = box[4]
+        color = color_dict[box[5]]
+        im = cv.rectangle(im, pt1, pt2, color, 2)
+        if id not in drawn_ids:
+            if output is not None:
+                output.write(str(fcount)+', '+str(float(id))+', '+str(float(box[0]))+', '+str(float(box[1]))+', '+str(float(
+                             box[2]-box[0]))+', '+str(float(box[3]-box[1])) + ', -1, -1, -1, -1\n')
+            drawn_ids.append(id)
+        cv.putText(im, str(id) + ': ' + label_dict[label], text_pos, cv.FONT_HERSHEY_SIMPLEX, 0.5, color, 1)
     return im
 
 
 class DetectionSystem:
 
-    def __init__(self, input_path, model='yolov5m', size=640, half=True, mode='deep_sort', frame_count=1000, show=True,
-                 show_framerate=True, crop=True, scale=1.0):
-        self.detector = yolo_detector.YoloDetector(model, size=size, half=half, mode=mode)
-        self.frame_count = frame_count
-        self.capture = cv.VideoCapture(input_path)
+    def __init__(self, detector, input_path, output, is_video=False, show=True, show_framerate=True):
+        self.detector = detector
+        self.input_path = input_path
+        self.is_video = is_video
         self.show = show
         self.show_framerate = show_framerate
-        self.crop = crop
-        self.scale = scale
+
         self.frame_rate = []
         self.frame_processing_time = []
+        self.output = output
 
     def run(self):
-        _, init_frame = self.capture.read()
-        W = int(init_frame.shape[1] * self.scale)
-        H = int(init_frame.shape[0] * self.scale)
-        frame_count = 0
-
+        frame_count = 1
         if self.show:
-            cv.namedWindow('window')
-
-        if self.crop:
-            new_boundaries.clear()
-            cv.setMouseCallback('window', append_click_pos)
+            cv.namedWindow('Detector')
+        if self.is_video:
+            capture = cv.VideoCapture(self.input_path)
+            prev_frame_time = 0
             while True:
-                if len(new_boundaries) == 1:
+                new_frame_time = time.time()
+                fps = 1 / (new_frame_time - prev_frame_time)
+                self.frame_rate.append(fps)
+                prev_frame_time = new_frame_time
+                _, frame = capture.read()
+                if frame is None:
                     break
-                cv.imshow('window', cv.resize(init_frame, (W, H)))
-                k = cv.waitKey(20) & 0xFF
-                if k == 27:
-                    break
-
-            while True:
-                if len(new_boundaries) == 2:
-                    break
-                cv.imshow('window', cv.resize(init_frame, (W, H)))
-                k = cv.waitKey(20) & 0xFF
-                if k == 27:
-                    break
-
-            if len(new_boundaries) != 2:
-                self.capture.release()
-                cv.destroyAllWindows()
-
-            if new_boundaries[0][0] < new_boundaries[1][0] and new_boundaries[0][1] > new_boundaries[1][1]:
-                new_boundaries.append(new_boundaries[0])
-                new_boundaries.pop(0)
-
-            print("Upper Right Coordinate: ", new_boundaries[0])
-            print("Bottom Left Coordinate: ", new_boundaries[1])
-        else:
-            new_boundaries.append((0, 0))
-            new_boundaries.append((0, 0))
-
-        prev_frame_time = 0
-
-        while True:
-            if frame_count == self.frame_count:
-                break
-
-            new_frame_time = time.time()
-            fps = 1 / (new_frame_time - prev_frame_time)
-            self.frame_rate.append(fps)
-            prev_frame_time = new_frame_time
-
-            _, frame = self.capture.read()
-            frame = cv.resize(frame, (W, H))
-
-            if self.crop:
-                urx, ury = new_boundaries[0]
-                blx, bly = new_boundaries[1]
-
-                cropped = frame[ury:bly, blx:urx, :]
-
-                detect_start_time = time.time()
-                bboxes = self.detector.detect(cropped)
-                self.frame_processing_time.append(time.time() - detect_start_time)
-
-                frame = cv.rectangle(frame, new_boundaries[0], new_boundaries[1], (255, 255, 255), 1)
-                if len(bboxes) > 0:
-                    frame = draw_bbox(frame, bboxes, blx, ury)
-            else:
                 detect_start_time = time.time()
                 bboxes = self.detector.detect(frame)
                 self.frame_processing_time.append(time.time() - detect_start_time)
                 if len(bboxes) > 0:
-                    frame = draw_bbox(frame, bboxes, 0, 0)
-
-            if self.show_framerate:
-                cv.putText(frame, str(int(fps)), (7, 70), cv2.FONT_HERSHEY_SIMPLEX, 3, (100, 255, 0), 3, cv2.LINE_AA)
-
-            if self.show:
-                cv.imshow('window', frame)
-
-            if cv.waitKey(1) & 0xFF == 27:
-                break
-
-            frame_count += 1
-
-        self.capture.release()
-        cv.destroyAllWindows()
+                    frame = draw_bbox(frame, bboxes, self.output, frame_count)
+                if self.show_framerate:
+                    cv.putText(frame, str(int(fps)), (7, 70), cv.FONT_HERSHEY_SIMPLEX, 3, (100, 255, 0), 3, cv.LINE_AA)
+                if self.show:
+                    cv.imshow('Detector', frame)
+                if cv.waitKey(1) & 0xFF == 27:
+                    break
+                frame_count += 1
+            capture.release()
+            cv.destroyAllWindows()
+        else:
+            prev_frame_time = 0
+            for framedir in os.listdir(self.input_path):
+                frame = cv.imread(self.input_path + framedir)
+                new_frame_time = time.time()
+                fps = 1 / (new_frame_time - prev_frame_time)
+                self.frame_rate.append(fps)
+                prev_frame_time = new_frame_time
+                detect_start_time = time.time()
+                bboxes = self.detector.detect(frame)
+                self.frame_processing_time.append(time.time() - detect_start_time)
+                if len(bboxes) > 0:
+                    frame = draw_bbox(frame, bboxes, self.output, frame_count)
+                if self.show_framerate:
+                    cv.putText(frame, str(int(fps)), (7, 70), cv.FONT_HERSHEY_SIMPLEX, 3, (100, 255, 0), 3, cv.LINE_AA)
+                if self.show:
+                    cv.imshow('Detector', frame)
+                if cv.waitKey(1) & 0xFF == 27:
+                    break
+                frame_count += 1
+            cv.destroyAllWindows()
+        if self.output is not None:
+            self.output.close()
--- yolo_detector.py
+++ yolo_detector.py
@@ -1,19 +1,140 @@
 import os
 import torch
+import cv2 as cv
 import numpy as np
+from sort.sort import Sort
 from models import experimental as exp
 from utils import general, datasets
 from deepsort.utils.parser import get_config
 from deepsort.deep_sort import DeepSort
 from utils.metrics import bbox_iou
+from yolov3.pytorchyolo import models, detect
 
 
 def is_not_intersect(cur_bbox, bbox):
     return bbox[2] < cur_bbox[0] or bbox[0] > cur_bbox[2] or bbox[1] > cur_bbox[3] or bbox[3] < cur_bbox[1]
 
 
-class YoloDetector:
-    def __init__(self, model_name, size=640, half=False, conf_thres=0.3, iou_thres=0.55, mode='iou'):
+class YoloV3Detector:
+    def __init__(self, conf_thres=0.2, iou_thres=0.2, mode='deep_sort'):
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model = models.load_model("yolov3/config/yolov3.cfg", "yolov3/weights/yolov3.weights")
+        self.conf_thres = conf_thres
+        self.iou_thres = iou_thres
+        self.mode = mode
+        config = get_config()
+        config.merge_from_file("deepsort/configs/deep_sort.yaml")
+        self.tracker = None
+        if self.mode == 'deep_sort':
+            self.tracker = DeepSort(config.DEEPSORT.REID_CKPT,
+                                       max_dist=config.DEEPSORT.MAX_DIST,
+                                       min_confidence=config.DEEPSORT.MIN_CONFIDENCE,
+                                       max_iou_distance=config.DEEPSORT.MAX_IOU_DISTANCE,
+                                       max_age=config.DEEPSORT.MAX_AGE,
+                                       n_init=config.DEEPSORT.N_INIT,
+                                       nn_budget=config.DEEPSORT.NN_BUDGET,
+                                       use_cuda=True)
+        elif self.mode == 'sort':
+            self.tracker = Sort()
+        elif self.mode == 'iou':
+            self.id = 1
+            self.cur_bboxes = None
+            self.skipped_bboxes = {}
+
+    def detect(self, input_img):
+        img0 = input_img.copy()
+        img = cv.cvtColor(input_img, cv.COLOR_BGR2RGB)
+        prediction = detect.detect_image(self.model, img)
+        bbox_xywh = []
+        confidences = []
+        classes = []
+        if len(prediction) > 0:
+            # x1 y1 x2 y2 conf lbl
+            if self.mode == 'deep_sort':
+                for x1, y1, x2, y2, conf, lbl in prediction:
+                    label = int(lbl)
+                    if label in [0, 1, 2, 3, 5, 7]:
+                        bbox_xywh.append([0.5 * (x1 + x2), 0.5 * (y1 + y2), x2 - x1, y2 - y1])
+                        confidences.append(conf)
+                        classes.append(label)
+                bbox_xywh = torch.Tensor(bbox_xywh)
+                confidences = torch.Tensor(confidences)
+                classes = torch.Tensor(classes)
+                if len(bbox_xywh):
+                    deep_sort_output = self.tracker.update(bbox_xywh, confidences, classes, img0)
+                    return deep_sort_output
+
+            elif self.mode == 'iou':
+                if self.cur_bboxes is None:
+                    self.cur_bboxes = {}
+                    for x1, y1, x2, y2, conf, lbl in prediction:
+                        label = int(lbl)
+                        if label in [0, 1, 2, 3, 5, 7]:
+                            self.cur_bboxes[(x1, y1, x2, y2)] = [self.id, label]
+                            self.id += 1
+                    return []
+                else:
+                    new_bboxes = {}
+                    for x1, y1, x2, y2, conf, lbl in prediction:
+                        label = int(lbl)
+                        if label in [0, 1, 2, 3, 5, 7]:
+                            bbox = (x1, y1, x2, y2)
+                            if bbox in self.cur_bboxes:
+                                new_bboxes[bbox] = self.cur_bboxes[bbox]
+                                self.cur_bboxes.pop(bbox)
+                            else:
+                                match_found = False
+                                max_iou = 0
+                                best_box = None
+                                for cur_bbox in list(self.cur_bboxes):
+                                    if is_not_intersect(cur_bbox, bbox):
+                                        continue
+                                    iou = bbox_iou(torch.Tensor(np.asarray(bbox)), torch.Tensor(np.asarray(cur_bbox)))
+
+                                    if iou > max_iou:
+                                        max_iou = iou
+                                        best_box = cur_bbox
+                                if max_iou > 0.3:
+                                    new_bboxes[bbox] = self.cur_bboxes[best_box]
+                                    self.cur_bboxes.pop(best_box)
+                                    match_found = True
+
+                                if not match_found:
+                                    for skip_bbox in list(self.skipped_bboxes):
+                                        if is_not_intersect(skip_bbox, bbox):
+                                            continue
+                                        iou = bbox_iou(torch.Tensor(np.asarray(bbox)), torch.Tensor(np.asarray(skip_bbox)))
+                                        if float(iou) > 0.3:
+                                            new_bboxes[bbox] = self.skipped_bboxes[skip_bbox]
+                                            self.skipped_bboxes.pop(skip_bbox)
+                                            break
+                                if bbox not in new_bboxes:
+                                    new_bboxes[bbox] = [self.id, label]
+                                    self.id += 1
+
+                    self.cur_bboxes = new_bboxes
+                    output = []
+                    for bbox in list(self.cur_bboxes):
+                        tup = self.cur_bboxes[bbox]
+                        output.append(list(bbox) + tup)
+                    return output
+            elif self.mode == 'sort':
+                dets = []
+
+                sort_output = []
+                for x1, y1, x2, y2, conf, lbl in prediction:
+                    label = int(lbl)
+                    if label in [0, 1, 2, 3, 5, 7]:
+                        dets.append([x1, y1, x2, y2, conf])
+                detections = self.tracker.update(np.asarray(dets))
+                for bbox in detections:
+                    sort_output.append(list(bbox) + [0])
+                return sort_output
+        return []
+
+
+class YoloV5Detector:
+    def __init__(self, model_name='yolov5l', size=640, half=True, conf_thres=0.2, iou_thres=0.2, mode='deep_sort'):
         self.weights = os.path.join("weights/", model_name + ".pt")
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.size = size
@@ -65,11 +186,10 @@ class YoloDetector:
         if len(prediction) > 0:
             # x1 y1 x2 y2 conf lbl
             prediction[:, :4] = general.scale_coords(img.shape[2:], prediction[:, :4], img0.shape).round()
-
             if self.mode == 'deep_sort':
                 for *coord, confidence, cls in prediction:
                     label = int(cls)
-                    if label in [2, 3, 5, 7]:
+                    if label in [0, 1, 2, 3, 5, 7]:
                         x1 = int(coord[0])
                         y1 = int(coord[1])
                         x2 = int(coord[2])
@@ -84,13 +204,12 @@ class YoloDetector:
                 if len(bbox_xywh):
                     deep_sort_output = self.ds_tracker.update(bbox_xywh, confidences, classes, img0)
                     return deep_sort_output
-
             elif self.mode == 'iou':
                 if self.cur_bboxes is None:
                     self.cur_bboxes = {}
                     for *coord, _, cls in prediction:
                         label = int(cls)
-                        if label in [2, 3, 5, 7]:
+                        if label in [0, 1, 2, 3, 5, 7]:
                             x1 = int(coord[0])
                             y1 = int(coord[1])
                             x2 = int(coord[2])
@@ -102,7 +221,7 @@ class YoloDetector:
                     new_bboxes = {}
                     for *coord, _, cls in prediction:
                         label = int(cls)
-                        if label in [2, 3, 5, 7]:
+                        if label in [0, 1, 2, 3, 5, 7]:
                             x1 = int(coord[0])
                             y1 = int(coord[1])
                             x2 = int(coord[2])
@@ -112,32 +231,39 @@ class YoloDetector:
                                 new_bboxes[bbox] = self.cur_bboxes[bbox]
                                 self.cur_bboxes.pop(bbox)
                             else:
+                                match_found = False
+                                max_iou = 0
+                                best_box = None
                                 for cur_bbox in list(self.cur_bboxes):
                                     if is_not_intersect(cur_bbox, bbox):
                                         continue
                                     iou = bbox_iou(torch.Tensor(np.asarray(bbox)), torch.Tensor(np.asarray(cur_bbox)))
-                                    if float(iou) > 0.45:
-                                        new_bboxes[bbox] = self.cur_bboxes[cur_bbox]
-                                        self.cur_bboxes.pop(cur_bbox)
-                                        break
-                                    self.skipped_bboxes[cur_bbox] = self.cur_bboxes[cur_bbox]
-                                for skip_bbox in list(self.skipped_bboxes):
-                                    if is_not_intersect(skip_bbox, bbox):
-                                        continue
-                                    iou = bbox_iou(torch.Tensor(np.asarray(bbox)), torch.Tensor(np.asarray(skip_bbox)))
-                                    if float(iou) > 0.45:
-                                        new_bboxes[bbox] = self.skipped_bboxes[skip_bbox]
-                                        self.skipped_bboxes.pop(skip_bbox)
-                                        break
+
+                                    if iou > max_iou:
+                                        max_iou = iou
+                                        best_box = cur_bbox
+                                if max_iou > 0.3:
+                                    new_bboxes[bbox] = self.cur_bboxes[best_box]
+                                    self.cur_bboxes.pop(best_box)
+                                    match_found = True
+
+                                if not match_found:
+                                    for skip_bbox in list(self.skipped_bboxes):
+                                        if is_not_intersect(skip_bbox, bbox):
+                                            continue
+                                        iou = bbox_iou(torch.Tensor(np.asarray(bbox)),
+                                                       torch.Tensor(np.asarray(skip_bbox)))
+                                        if float(iou) > 0.3:
+                                            new_bboxes[bbox] = self.skipped_bboxes[skip_bbox]
+                                            self.skipped_bboxes.pop(skip_bbox)
+                                            break
                                 if bbox not in new_bboxes:
                                     new_bboxes[bbox] = [self.id, label]
                                     self.id += 1
-                                if len(self.skipped_bboxes) > 5:
-                                    self.skipped_bboxes.clear()
                     self.cur_bboxes = new_bboxes
                     output = []
                     for bbox in list(self.cur_bboxes):
                         tup = self.cur_bboxes[bbox]
                         output.append(list(bbox) + tup)
                     return output
-        return []
+            return []
